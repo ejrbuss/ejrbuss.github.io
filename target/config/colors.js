c9a02c215ee92d2fa3ee74126cc833e976561da7
ejrbuss <ejrbuss@gmail.com>
2019-05-15 17:44:36 -0700

    website 2.0

--- target/config/colors.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    background: '#000000',
-    foreground: '#FFFFFF',
-    secondary: '#e3e3e3',
-    accent: '#F44336'
-};
--- target/pages/blog.js
+++
@@ -1,8 +0,0 @@
-var colors = require('../config/colors');
-var domjs = require('domjs')(document);
-var blog = function () {
-    var _a = domjs.ns, div = _a.div, h2 = _a.h2;
-    document.body.appendChild(div({ id: 'content' }, h2('Work in Progress')));
-    $('#content').hide().fadeIn(500);
-};
-module.exports = blog;
